4a1de3e4d14f0f3175e3173d8fc38056bc37568e
Dave Rogers <thedude@yukondude.com>
2019-11-06 21:33:20 -0800

    Add sample option and argument and other common options.

--- mitfixins/cli.py
+++ mitfixins/cli.py
@@ -38,6 +38,23 @@ def show_version(ctx, param, value):
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.option(
+    "--option", default=42, show_default=True, type=int, help="A sample option."
+)
+@click.option(
+    "--dry-run",
+    "-D",
+    is_flag=True,
+    help="Show the intended operations but do not run them (implies --verbose).",
+)
+@click.option(
+    "--verbose",
+    "-v",
+    count=True,
+    help="Increase the verbosity of status messages: use once for normal output, twice "
+    "for additional output, and thrice for debug-level output.",
+)
+@click.argument("ARGUMENT", nargs=-1)
+@click.option(
     "--version",
     "-V",
     is_flag=True,
